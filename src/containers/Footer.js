81b2ea6e081c18cee8bff0d7ad4438950b6437fc
Sebastian Simarro <ssebastian210@gmail.com>
2021-09-04 10:39:51 -0300

    <Footer><date> fix local

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -1,16 +1,34 @@
-import { Box, Spacer } from '@chakra-ui/layout';
+import { Box, Spacer, Stack } from '@chakra-ui/layout';
 import { Flex, Text } from '@chakra-ui/react';
 import React from 'react';
 
 const Footer = () => {
+  const d = new Date();
+  let options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
+  const fecha = d.toLocaleString('es-US', options);
   return (
-    <Box h="12vh" borderTop="1px" borderTopColor="gray.400">
-      <Flex justifyContent="space-around">
-        <Text>Fecha</Text>
-        <Spacer />
-        <Text>Autenticado por Administración General De Ingresos Públicos</Text>
-      </Flex>
-    </Box>
+    <Stack
+      direction={['column', 'row']}
+      h="10vh"
+      spacing="24px"
+      borderTop="1px"
+      borderTopColor="gray.400"
+      justifyContent="space-between"
+      padding="3"
+      alignItems="center"
+    >
+      <Text fontSize="small" fontWeight="semibold">
+        {fecha}
+      </Text>
+      <Text fontSize="small" fontWeight="semibold">
+        Autenticado por Administración General De Ingresos Públicos
+      </Text>
+    </Stack>
   );
 };
 
