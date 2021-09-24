48f009bd3a6b3e10503eb412065d541272cc9775
Sebastian Simarro <ssebastian210@gmail.com>
2021-09-24 10:45:08 -0300

    <refactor>delete branches + vh al main

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -2,21 +2,15 @@ import React from 'react';
 import { Stack } from '@chakra-ui/layout';
 import MainBox from '../components/MainBox';
 import UserBox from '../components/UserBox';
-import UserBoxFull from '../components/UserBoxFull';
-import MainBoxFull from '../components/MainBoxFull';
-import PtosDeVenta from '../pages/PtosDeVenta';
-import MenuPpal from '../pages/MenuPpal';
 
 const Main = () => {
   return (
     <Stack
       direction={['column', 'row']}
       spacing="15px"
-      padding={4}
       justifyContent="space-evenly"
       alignItems="center"
-      height="auto"
-      mb="12"
+      height="80vh"
     >
       {/* <UserBoxFull />
       <MenuPpal /> */}
